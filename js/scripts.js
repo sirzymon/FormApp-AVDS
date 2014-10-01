f77c6eef7e4dff1a86e69dcddb3917edfe16b17e
Zymon <zymon@Queens.local>
2014-10-01 14:56:34 +0800

    added battery statu api

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,33 @@
-$(document).on('pageinit', '#home', function(){     
-    var url = 'http://api.themoviedb.org/3/',
-        mode = 'search/movie?query=',
-        movieName = '&query='+encodeURI('Batman'),       
-        key = '&api_key=470fd2ec8853e25d2f8d86f685d2270e';       
-     
-    $.ajax({
-        url: url + mode + key + movieName ,
-        dataType: "jsonp",
-        async: true,
-        success: function (result) {
-            ajax.parseJSONP(result);
-        },
-        error: function (request,error) {
-            alert('Network error has occurred please try again!');
-        }
-    });        
-});
- 
-$(document).on('pagebeforeshow', '#headline', function(){     
-    $('#movie-data').empty();
-    $.each(movieInfo.result, function(i, row) {
-        if(row.id == movieInfo.id) {
-            $('#movie-data').append('<li><img src="http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w185'+row.poster_path+'"></li>');
-            $('#movie-data').append('<li>Title: '+row.original_title+'</li>');
-            $('#movie-data').append('<li>Release date'+row.release_date+'</li>');
-            $('#movie-data').append('<li>Popularity : '+row.popularity+'</li>');  
-            $('#movie-data').append('<li>Popularity : '+row.vote_average+'</li>');            
-            $('#movie-data').listview('refresh');           
-        }
-    });   
-});
- 
-$(document).on('vclick', '#movie-list li a', function(){ 
-    movieInfo.id = $(this).attr('data-id');
-    $.mobile.changePage( "#headline", { transition: "slide", changeHash: false });
-});
- 
-var movieInfo = {
-    id : null,
-    result : null
+//BATTERY STATUS API START
+window.navigator = window.navigator || {};
+navigator.battery = navigator.battery || null;
+if (navigator.battery === null) {
+	document.getElementById('bs-unsupported').classList.remove('hidden');
+} else {
+	var log = document.getElementById('log');
+
+	document.getElementById('bt-results').classList.remove('hidden');
+	function updateInfo(event) {
+		if (event !== undefined) {
+			log.innerHTML = 'Event "' + event.type + '" fired<br />'
+					+ log.innerHTML;
+		}
+		document.getElementById('in-charge').innerHTML = (navigator.battery.charging ? "Yes"
+				: "No");
+		document.getElementById('charging-time').innerHTML = navigator.battery.chargingTime;
+		document.getElementById('discharging-time').innerHTML = navigator.battery.dischargingTime;
+		document.getElementById('battery-level').innerHTML = navigator.battery.level;
+	}
+
+	var events = [ 'chargingchange', 'chargingtimechange',
+			'dischargingtimechange', 'levelchange' ];
+	for ( var i = 0; i < events.length; i++) {
+		navigator.battery.addEventListener(events[i], updateInfo);
+	}
+	updateInfo();
+
+	document.getElementById('clear-log').addEventListener('click', function() {
+		log.innerHTML = '';
+	});
 }
- 
-var ajax = { 
-    parseJSONP:function(result){ 
-        movieInfo.result = result.results;
-        $.each(result.results, function(i, row) {
-            console.log(JSON.stringify(row));
-            $('#movie-list').append('<li><a href="" data-id="' + row.id + '"><img src="http://d3gtl9l2a4fn1j.cloudfront.net/t/p/w185'+row.poster_path+'"/><h3>' + row.title + '</h3><p>' + row.vote_average + '/10</p></a></li>');
-        });
-        $('#movie-list').listview('refresh');
-    }
-}
+//BATTERY STATUS API END
